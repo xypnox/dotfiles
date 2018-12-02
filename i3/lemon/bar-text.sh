708526b6ad4924303cf73e6e8af5c7e9ec6c3f88
xypnox <xypnox@gmail.com>
2018-12-02 20:40:22 +0530

    Made minor tweaks

---
+++ i3/lemon/bar-text.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+clock() {
+    date +%H:%M:%S
+}
+
+battery() {
+    cat /sys/class/power_supply/BAT0/capacity
+}
+
+
+BAR_INPUT="%{c}LIFE : $(battery)%% TIME : $(clock)"
+echo $BAR_INPUT
