7cec638e259957260cff077fa47ec93b5b914eab
xypnox <xypnox@gmail.com>
2021-01-04 15:21:37 +0530

    Add conky and start script

---
+++ conky/start_conky.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+conky -c /home/xypnox/dotfiles/conky/conky_xypnox &&
+
+exit 0
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,7 +5,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -r example &
+polybar example &
 
 echo "Polybar launched..."
 
