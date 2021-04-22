d980da9a3bff72570395c491a4ee878eacd5ea77
xypnox <xypnox@gmail.com>
2021-04-23 02:53:56 +0530

    Firefox fullscreen, rectscreenshot cursor, dendron keybinding
    Firefox now hides tabbar and urlbar in fullscreen for complete immersion
    
    Rect screenshots no longer have the cursor in them.
    
    Added keybinding for dendron workspace for notetaking.

--- scripts/screenshot_rect.sh
+++ scripts/screenshot_rect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-maim -s $HOME/Pictures/rect/$(date +%F-%a-%H-%M_ns%N).png &&
+maim -u -s $HOME/Pictures/rect/$(date +%F-%a-%H-%M_ns%N).png &&
 notify-send -t 1000 "Screenshot has been saved."
