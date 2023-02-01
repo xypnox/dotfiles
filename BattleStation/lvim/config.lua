807ff38150a6c449b2d4477d834c6e4f8d85284b
xypnox <xypnox@gmail.com>
2023-02-01 16:26:49 +0530

    Add zen mode to lunarvim

--- BattleStation/lvim/config.lua
+++ BattleStation/lvim/config.lua
@@ -197,6 +197,27 @@ lvim.plugins = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
+            window = {
+        --backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+        -- height and width can be:
+        -- * an absolute number of cells when > 1
+        -- * a percentage of the width / height of the editor when <= 1
+        -- * a function that returns the width or the height
+        width = 80, -- width of the Zen window
+        -- height = 40, -- height of the Zen window
+        -- by default, no options are changed for the Zen window
+        -- uncomment any of the options below, or add other vim.wo options you want to apply
+        options = {
+            signcolumn = "no", -- disable signcolumn
+            number = false, -- disable number column
+            relativenumber = false, -- disable relative numbers
+            cursorline = false, -- disable cursorline
+            -- cursorcolumn = false, -- disable cursor column
+            -- foldcolumn = "0", -- disable fold column
+            -- list = false, -- disable whitespace characters
+            },
+        },
+
       }
     end
   }
